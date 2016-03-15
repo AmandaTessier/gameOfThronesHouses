7bd8df0774ac23bfe1abdb2bd9066f66ef0cbbae
AmandaTessier <atessier2012@gmail.com>
2016-03-15 15:32:36 -0400

    Homework Assignment

---
+++ gamesofThronesHouses.js
@@ -0,0 +1,20 @@
+gameOfThronesHouses[0].name
+"Starks"
+gameOfThronesHouses[10].name
+"Dothrakis"
+gameOfThronesHouses[3].wikiSuffix
+"House_Targaryen"
+gameOfThronesHouses[9].wikiSuffix
+"House_Arryns"
+gameOfThronesHouses[2].people[0]
+Object {name: "King Robert Baratheon", description: "The first of his name - Lord of the Seven Kingdoms", imageSuffix: "robert-baratheon", wikiSuffix: "Robert_Baratheon"}
+gameOfThronesHouses[6].people[2].description
+"Heir to Riverrun - Son of Hoster Tully"
+gameOfThronesHouses[7].people[0].imageSuffix
+"olenna-tyrell"
+gameOfThronesHouses[0]//people
+Object {name: "Starks", wikiSuffix: "House_Stark", people: Array[8]}
+gameOfThronesHouses[4]//people.number
+Object {name: "Greyjoys", wikiSuffix: "House_Greyjoy", people: Array[3]}
+gameOfThronesHouses[3]//people.number
+Object {name: "Targaryens", wikiSuffix: "House_Targaryen", people: Array[2]}
